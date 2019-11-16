6b4f7e09e26452cd2de356349a9eab192719233c
jorgesancheznet <info@jorgesanchez.net>
2019-11-16 11:36:24 +0100

    Buscaminas arreglando bugs

--- unidad-08/practica-08-12/buscaminas.js
+++ unidad-08/practica-08-12/buscaminas.js
@@ -2,6 +2,7 @@
 //función constructora
 function Buscaminas(ancho, alto, nMinas) {
     const HAY_MINA = 10;
+    const SIMBOLO_MINA = "&#128163;";
     //propiedades privadas
     var _ancho = ancho;
     var _alto = alto;
@@ -10,10 +11,14 @@ function Buscaminas(ancho, alto, nMinas) {
     var _celdasDestapadas=0;
     var _celdasSinMina=ancho*alto-nMinas;
     //validamos la corrección de los datos
-    if (isNaN(Number(parseInt(ancho))) || ancho == 0) return null;
-    if (isNaN(Number(parseInt(alto))) || alto == 0) return null;
-    if (isNaN(Number(parseInt(nMinas))) || nMinas == 0) return null;
-    if (ancho * alto < nMinas) return null;
+    if (isNaN(Number(parseInt(ancho))) || ancho === 0)
+        return null;
+    if (isNaN(Number(parseInt(alto))) || alto === 0)
+        return null;
+    if (isNaN(Number(parseInt(nMinas))) || nMinas === 0)
+        return null;
+    if (ancho * alto < nMinas)
+        return null;
 
 
 
@@ -40,41 +45,41 @@ function Buscaminas(ancho, alto, nMinas) {
      * Mira las celdas de alrededor y coloca el número de minas en 
      * la celda cuya posición se indica
      */
-    _colocaNúmeroCelda = function (fila, col) {
+    _colocaNumeroCelda = function (fila, col) {
         let cont = 0;
         //solo cambiamos las celdas sin mina
-        if (_tablero[fila][col] != HAY_MINA) {
+        if (_tablero[fila][col] !== HAY_MINA) {
             //revisamos las ocho celdas alrededor
             if (col > 0) {
                 if (fila > 0) {
-                    if (_tablero[fila - 1][col - 1] == HAY_MINA)
+                    if (_tablero[fila - 1][col - 1] === HAY_MINA)
                         cont++;
                 }
-                if (_tablero[fila][col - 1] == HAY_MINA)
+                if (_tablero[fila][col - 1] === HAY_MINA)
                     cont++;
                 if (fila < _tablero.length - 1) {
-                    if (_tablero[fila + 1][col - 1] == HAY_MINA)
+                    if (_tablero[fila + 1][col - 1] === HAY_MINA)
                         cont++;
                 }
             }
             if (col < _tablero[fila].length) {
                 if (fila > 0) {
-                    if (_tablero[fila - 1][col + 1] == HAY_MINA)
+                    if (_tablero[fila - 1][col + 1] === HAY_MINA)
                         cont++;
                 }
-                if (_tablero[fila][col + 1] == HAY_MINA)
+                if (_tablero[fila][col + 1] === HAY_MINA)
                     cont++;
                 if (fila < _tablero.length - 1) {
-                    if (_tablero[fila + 1][col + 1] == HAY_MINA)
+                    if (_tablero[fila + 1][col + 1] === HAY_MINA)
                         cont++;
                 }
             }
             if (fila > 0) {
-                if (_tablero[fila - 1][col] == HAY_MINA)
+                if (_tablero[fila - 1][col] === HAY_MINA)
                     cont++;
             }
             if (fila < _tablero.length - 1) {
-                if (_tablero[fila + 1][col] == HAY_MINA)
+                if (_tablero[fila + 1][col] === HAY_MINA)
                     cont++;
             }
             _tablero[fila][col] = cont;
@@ -84,11 +89,11 @@ function Buscaminas(ancho, alto, nMinas) {
      * Pone los números de mina alrededor de
      * todas las celdas
      */
-    _colocaNúmeros = function () {
+    _colocaNumeros = function () {
         //recorremos las celdas
         for (let i = 0; i < _alto; i++) {
             for (let j = 0; j < _ancho; j++) {
-                _colocaNúmeroCelda(i, j);
+                _colocaNumeroCelda(i, j);
             }
         }
     };
@@ -101,7 +106,7 @@ function Buscaminas(ancho, alto, nMinas) {
             do {
                 fila = parseInt(Math.random() * _alto);
                 col = parseInt(Math.random() * _ancho);
-            } while (_tablero[fila][col] == HAY_MINA);
+            } while (_tablero[fila][col] === HAY_MINA);
             //colocamos la mina
             _tablero[fila][col] = HAY_MINA;
         }
@@ -117,22 +122,24 @@ function Buscaminas(ancho, alto, nMinas) {
         if(celda.classList.contains("oculta")){
             celda.classList.remove("oculta");
             celda.classList.add("visible");
-            //leemos las coordenadas del tablero
-            //y las cuadramos con el tablero
+            //leemos coordenadas del tablero relacionadas
+            //con la celda
             let i=celda.dataset.i;
             let j=celda.dataset.j;
-            celda.textContent=_tablero[i][j];
             //fin del juego si hay una mina
-            if(_tablero[i][j]==HAY_MINA){
+            if(_tablero[i][j]===HAY_MINA){
+                celda.classList.remove("oculta");
+                celda.classList.add("visible");
+                celda.classList.add("mina");
                 alert("OOOOOHH! EL juego ha finalizado. ¡Has pisado una mina!");
             }
             else {
                 _celdasDestapadas++;
-                if(_celdasDestapadas==_celdasSinMina){
+                if(_celdasDestapadas===_celdasSinMina){
                     //hemos destapado todas las celdas
                     alert("¡ENHORABUENA, has ganado!");
                 }
-                if(_tablero[i][j]==0){
+                if(_tablero[i][j]===0){
                     celda.innerHTML="&nbsp;";
                 }
             }
@@ -145,13 +152,13 @@ function Buscaminas(ancho, alto, nMinas) {
     this.recalcular = function (ancho, alto, nMinas) {
         //Si no se pasan parámetros, se toman
         //los valores antiguos
-        if (ancho != undefined) _ancho = ancho;
-        if (alto != undefined) _alto = alto;
-        if (nMinas != undefined) _nMinas = nMinas;
+        if (ancho !== undefined) _ancho = ancho;
+        if (alto !== undefined) _alto = alto;
+        if (nMinas !== undefined) _nMinas = nMinas;
         _comenzarTablero();
         _colocaMinas();
-        _colocaNúmeros();
-    }
+        _colocaNumeros();
+    };
 
     /**
      * Prepara el tablero de minas
@@ -166,30 +173,27 @@ function Buscaminas(ancho, alto, nMinas) {
                 let celda = document.createElement("div");
                 celda.classList.add("celda");
                 celda.classList.add("oculta");
-                celda.textContent="&nbsp;"
+                celda.textContent = "&nbsp;";
                 //anotamos la posición en el tablero de la celda
-                celda.dataset.i=i;
-                celda.dataset.j=j;
+                celda.dataset.i = i;
+                celda.dataset.j = j;
+                //preparar contenido
+                if(_tablero[i][j]===HAY_MINA){
+                    celda.innerHTML="<span>"+SIMBOLO_MINA+"</span>";
+                }
+                else{
+                    celda.innerHTML="<span>"+_tablero[i][j]+"</span>";
+                }
                 //preparamos el evento para la celda
-                celda.addEventListener("click",(e)=>{
+                celda.addEventListener("click", (e) => {
                     _mostrarCelda(e.target);
                 });
                 //colocamos la celda
                 fila.appendChild(celda);
-                /*if (_tablero[i][j] > 0) {
-                    if (_tablero[i][j] == HAY_MINA) {
-                        celda.textContent="M";
-                    }
-                    else {
-                        celda.textContent=_tablero[i][j];
-                    }
-                }
-                else{
-                    celda.innerHTML="&nbsp;";
-                }*/
             }
+
         }
-    }
+    };
 
     this.recalcular();
 
